ec88c39e9628a5676d932a9f47c9f48919cb16a2
alxzgl1 <a.zhigalov@gmail.com>
2022-12-05 10:04:53 +0000

    L15

--- example_watershed_segmentation.m
+++ example_watershed_segmentation.m
@@ -26,8 +26,6 @@ rgb = I;
 
 I = rgb2gray(rgb);
 subplot(2, 4, 1); imshow(I)
-text(732,501,'Image courtesy of Corel(R)',...
-     'FontSize',7,'HorizontalAlignment','right')
 
 % Step 2: Use the Gradient Magnitude as the Segmentation Function
 gmag = imgradient(I);
@@ -43,22 +41,22 @@ title('Opening')
 Ie = imerode(I,se);
 Iobr = imreconstruct(Ie,I);
 subplot(2, 4, 4); imshow(Iobr)
-title('Opening-by-Reconstruction')
+title('Opening-by-Recon.')
 
 Iobrd = imdilate(Iobr,se);
 Iobrcbr = imreconstruct(imcomplement(Iobrd),imcomplement(Iobr));
 Iobrcbr = imcomplement(Iobrcbr);
 subplot(2, 4, 5); imshow(Iobrcbr)
-title('Opening-Closing by Reconstruction')
+title('Opening-Closing by Recon.')
 
 fgm = imregionalmax(Iobrcbr);
 subplot(2, 4, 6); imshow(fgm)
-title('Regional Maxima of Opening-Closing by Reconstruction')
+title('Maxima of Opening-Closing')
 
 % Step 4: Compute Background Markers
 bw = imbinarize(Iobrcbr);
 subplot(2, 4, 7); imshow(bw)
-title('Thresholded Opening-Closing by Reconstruction')
+title('Thresholded Opening-Closing')
 
 end % end
 
